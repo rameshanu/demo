1973238226b17b7c9e7da8e501e36cee11615a2f
rameshanu <javaramesh456@gmail.com>
2018-06-30 23:17:27 +0530

    55556556

--- src/com/rameshsoft/test/ArrayTest.java
+++ src/com/rameshsoft/test/ArrayTest.java
@@ -7,5 +7,8 @@ public static void main(String[] args) {
 	System.out.println("demo");
 	System.out.println("demo");
 	System.out.println("demo");
+	System.out.println("1");
+	System.out.println("2");
+	System.out.println("3");
 }
 }
