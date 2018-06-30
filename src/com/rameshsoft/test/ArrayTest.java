8ed5438a3868d8446e2bf276a446d09d85aba107
rameshanu <javaramesh456@gmail.com>
2018-06-30 23:26:14 +0530

    javaaaa

--- src/com/rameshsoft/test/ArrayTest.java
+++ src/com/rameshsoft/test/ArrayTest.java
@@ -11,6 +11,7 @@ public static void main(String[] args) {
 	System.out.println("2");
 	System.out.println("3");
 	System.out.println("123456");
-
+	System.out.println("Test123");
+	System.out.println("Test456");
 }
 }
